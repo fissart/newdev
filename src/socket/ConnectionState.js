16e9c3eeac2a795790440ccb084f34a9820ee347
root <root@www.esfapa.edu.pe>
2025-06-06 02:24:45 +0000

    w

--- src/socket/ConnectionState.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function ConnectionState({ isConnected }) {
-  return <>
-  {isConnected?<p style={{background:'orange'}}>Conectado</p>:
-  <p style={{background:'gray'}}>No conectado</p>}
-  </>;
-}
